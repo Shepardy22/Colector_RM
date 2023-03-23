507a58a463e8773279c8134c14a046dbdb4c8486
shepardy22 <inv72kmaster@gmail.com>
2023-03-23 07:01:24 -0300

    Connection database and implementations

--- CollectorManager/src/main/java/com/collector/collectormanager/CollectorManager.java
+++ CollectorManager/src/main/java/com/collector/collectormanager/CollectorManager.java
@@ -14,11 +14,21 @@ public class CollectorManager {
 
     public static void main(String[] args) {
         
+       
         SQLiteConnection db = new SQLiteConnection();
         
+        //Métodos implementados
+        /*
         db.connect();
         db.createTable();
         db.insertData();
+        db.updateData(0, "nome", 0);
+        db.retrieveData();
         db.disconnect();
+        */
+        
+        
+        
+        
     }
 }
--- CollectorManager/src/main/java/dbConnect/SQLiteConnection.java
+++ CollectorManager/src/main/java/dbConnect/SQLiteConnection.java
@@ -6,46 +6,47 @@ package dbConnect;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLiteConnection {
-    
+
     private Connection connection;
     private Statement statement;
-    
-    public boolean connect(){
+
+    public boolean connect() {
         String url = "jdbc:sqlite:data/database.db";
-        try{
+        try {
             //cria a conexão com o banco de dados
             this.connection = DriverManager.getConnection(url);
             System.out.println("Conexão com o banco de dados estabelecida.");
-            
+
             //cria o objeto Statement para execução de queries
             this.statement = this.connection.createStatement();
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.err.println(ex.getMessage());
             return false;
         }
         return true;
     }
-    
-    public boolean disconnect(){
-        try{
+
+    public boolean disconnect() {
+        try {
             //fecha a conexão com o banco de dados
             this.connection.close();
             System.out.println("Conexão com o banco de dados fechada.");
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.err.println(ex.getMessage());
             return false;
         }
         return true;
     }
-    
-    public boolean createTable(){
-        try{
+
+    public boolean createTable() {
+        try {
             //cria uma tabela no banco de dados
             String sql = "CREATE TABLE IF NOT EXISTS usuarios (\n"
                     + " id integer PRIMARY KEY,\n"
@@ -53,30 +54,64 @@ public class SQLiteConnection {
                     + " idade integer\n"
                     + ");";
             this.statement.execute(sql);
-            
+
             System.out.println("Tabela criada com sucesso.");
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.err.println(ex.getMessage());
             return false;
         }
         return true;
     }
-    
-    public boolean insertData(){
-        try{
+
+    public boolean insertData() {
+        try {
             //insere dados na tabela
-            String sql = "INSERT INTO usuarios(id, nome, idade) VALUES(1, 'João', 25);";
+            String sql = "INSERT INTO usuarios(id, nome, idade) VALUES(2, 'pedro', 32);";
             this.statement.executeUpdate(sql);
-            
+
             System.out.println("Dados inseridos com sucesso.");
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             System.err.println(ex.getMessage());
             return false;
         }
         return true;
     }
-    
-    
+
+    public boolean updateData(int id, String nome, int idade) {
+        try {
+            //atualiza dados na tabela
+            String sql = "UPDATE usuarios SET nome = '" + nome + "', idade = " + idade + " WHERE id = " + id + ";";
+            this.statement.executeUpdate(sql);
+
+            System.out.println("Dados atualizados com sucesso.");
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public void retrieveData() {
+        try {
+            // executa uma query para recuperar os dados da tabela
+            String sql = "SELECT id, nome, idade FROM usuarios;";
+            // percorre os resultados da query e imprime na tela
+            try (ResultSet rs = this.statement.executeQuery(sql)) {
+                // percorre os resultados da query e imprime na tela
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nome = rs.getString("nome");
+                    int idade = rs.getInt("idade");
+                    System.out.println("ID: " + id + " | Nome: " + nome + " | Idade: " + idade);
+                }
+            }
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
